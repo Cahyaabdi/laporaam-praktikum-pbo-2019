00370539f2174751bb6cd9964eb1279ede4c27de
Cahyaabdi <54808482+Cahyaabdi@users.noreply.github.com>
2019-10-14 22:30:57 +0700

    Add files via upload

---
+++ src/7_Overriding_dan_Overloading/TUGAS/Dosen1841720070yayak.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TUGAS;
+
+/**
+ *
+ * @author can
+ */
+
+    public class Dosen1841720070yayak extends Manusia1841720070yayak{
+    public void Makan1841720070yayak(){
+        System.out.println("Dosen Makan di Ruang Dosen");
+    }
+    public void Lembur(){
+        System.out.println("Dosen sering lembur di hari Sabtu dan Minggu");
+    }
+}
+
